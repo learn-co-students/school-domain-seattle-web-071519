efb311fce14b809ee687b3ef539677951c7f97f3
ayerest <irisiseve@gmail.com>
2019-07-17 16:17:49 -0700

    finished school domain lab

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,28 @@
 # code here!
+
+class School
+
+    attr_reader :roster, :name
+
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    def add_student(name, grade)
+        @roster[grade] ||= []
+        @roster[grade] << name
+    end
+
+    def grade(grade_num)
+        @roster[grade_num]
+    end
+
+    def sort
+        sorted = {}
+        @roster.each do |grade, students|
+            sorted[grade] = students.sort {|a,b| a<=>b}
+        end
+        sorted
+    end
+end
